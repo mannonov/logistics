eb4c28ca97ac89f225b459eae0afd8e414a0e2fa
mannonov <jahongir.sh.mannonov@gmail.com>
2022-11-20 17:07:49 +0500

    gson logic

--- app/src/main/java/com/myapp/logistics/model/Load.kt
+++ app/src/main/java/com/myapp/logistics/model/Load.kt
@@ -1,6 +1,9 @@
 package com.myapp.logistics.model
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.myapp.logistics.util.Constants
+import java.lang.reflect.Type
 
 data class Load(
     var aPoint: Point? = null,
@@ -10,12 +13,13 @@ data class Load(
     var description: String? = null
 ) {
 
-
+    private val gson = Gson()
+    private val pointType: Type = object : TypeToken<Point>() {}.type
 
     fun getHashMap(): HashMap<String, Any> {
         return hashMapOf(
-            Constants.A_POINT to aPoint.toString(),
-            Constants.B_POINT to bPoint.toString(),
+            Constants.A_POINT to gson.toJson(aPoint, pointType),
+            Constants.B_POINT to gson.toJson(bPoint, pointType),
             Constants.CUSTOMER to customer.toString(),
             Constants.DEADLINE to deadline.toString(),
             Constants.DESCRIPTION to description.toString()
