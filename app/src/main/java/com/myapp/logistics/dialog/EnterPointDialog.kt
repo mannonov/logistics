792067f4027b926f628443f873c0d9562bf3f9d5
mannonov <jahongir.sh.mannonov@gmail.com>
2022-11-21 01:33:01 +0500

    listeners added

--- app/src/main/java/com/myapp/logistics/dialog/EnterPointDialog.kt
+++ app/src/main/java/com/myapp/logistics/dialog/EnterPointDialog.kt
@@ -4,10 +4,13 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import com.myapp.logistics.databinding.EnterPointDialogBinding
+import com.myapp.logistics.model.Point
+import com.myapp.logistics.util.onClick
 
 class EnterPointDialog(context: Context) : Dialog(context) {
 
     private var binding: EnterPointDialogBinding? = null
+    private lateinit var addOnPointListener: PointListener
 
     override fun onStart() {
         super.onStart()
@@ -19,10 +22,28 @@ class EnterPointDialog(context: Context) : Dialog(context) {
         super.onCreate(savedInstanceState)
         binding = EnterPointDialogBinding.inflate(layoutInflater)
         binding?.root?.let { setContentView(it) }
+        binding?.btnLoad?.onClick {
+            if (this@EnterPointDialog::addOnPointListener.isInitialized) {
+                val point = Point()
+                binding?.apply {
+                    point.lat = tiePointLat.text.toString().toDouble()
+                    point.lng = tiePointLng.text.toString().toDouble()
+                    point.address = tiePointName.text.toString()
+                }
+                addOnPointListener.onAdd(point)
+                dismiss()
+            }
+        }
+    }
+
+    fun setAddOnPointListener(addOnPointListener: PointListener) {
+        this.addOnPointListener = addOnPointListener
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         binding = null
     }
+
+    class PointListener(val onAdd: (point: Point) -> Unit)
 }
--- app/src/main/java/com/myapp/logistics/ui/fragment/AddLoadFragment.kt
+++ app/src/main/java/com/myapp/logistics/ui/fragment/AddLoadFragment.kt
@@ -9,6 +9,7 @@ import com.myapp.logistics.R
 import com.myapp.logistics.databinding.FragmentAddLoadBinding
 import com.myapp.logistics.dialog.EnterPointDialog
 import com.myapp.logistics.model.Load
+import com.myapp.logistics.model.Point
 import com.myapp.logistics.util.onClick
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,6 +20,8 @@ private const val POINT_B = 1
 class AddLoadFragment : BottomSheetDialogFragment() {
 
     private var binding: FragmentAddLoadBinding? = null
+    private var pointA: Point? = null
+    private var pointB: Point? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_add_load, container, false)
@@ -40,7 +43,22 @@ class AddLoadFragment : BottomSheetDialogFragment() {
     }
 
     private fun showPointDialog(point: Int) {
-        EnterPointDialog(requireContext()).show()
+        EnterPointDialog(requireContext()).also { it ->
+            it.setAddOnPointListener(
+                addOnPointListener = EnterPointDialog.PointListener { selected_point ->
+                    when (point) {
+                        POINT_A -> {
+                            pointA = selected_point
+                            binding?.tieAPoint?.setText(selected_point.toString())
+                        }
+                        POINT_B -> {
+                            pointB = selected_point
+                            binding?.tieBPoint?.setText(selected_point.toString())
+                        }
+                    }
+                }
+            )
+        }.show()
     }
 
     private fun getLoadDataFromUI() {
