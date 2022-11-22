83b3ce9bb88a87fe6d1d8cfc1ae1b8d757dbef1f
mannonov <jahongir.sh.mannonov@gmail.com>
2022-11-23 00:40:17 +0500

    dialog logic implemented

--- app/src/main/java/com/myapp/logistics/dialog/LogisticDialog.kt
+++ app/src/main/java/com/myapp/logistics/dialog/LogisticDialog.kt
@@ -2,5 +2,39 @@ package com.myapp.logistics.dialog
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
+import com.myapp.logistics.databinding.LogisticsDialogBinding
+import com.myapp.logistics.util.onClick
 
-class LogisticDialog(context: Context, text: String) : Dialog(context)
+class LogisticDialog(context: Context, private val text: String) : Dialog(context) {
+
+    private lateinit var binding: LogisticsDialogBinding
+    private lateinit var yesClickListener: YesClickListener
+
+    override fun onStart() {
+        super.onStart()
+        val width = (context.resources.displayMetrics.widthPixels * 0.90).toInt()
+        this.window?.setLayout(width, -2)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LogisticsDialogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvContentText.text = text
+        binding.btnNo.onClick {
+            dismiss()
+        }
+        binding.btnYes.onClick {
+            if (this@LogisticDialog::yesClickListener.isInitialized) {
+                yesClickListener.onYes()
+            }
+        }
+    }
+
+    private fun setYesClickListener(yesClickListener: YesClickListener) {
+        this.yesClickListener = yesClickListener
+    }
+
+    class YesClickListener(val onYes: () -> Unit)
+}
