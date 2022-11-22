6f800f63e8cac17ef5b55f0d8d25237d5615b80d
mannonov <jahongir.sh.mannonov@gmail.com>
2022-11-22 21:24:40 +0500

    here and google maps implemented

--- app/src/main/java/com/myapp/logistics/app/LogisticsApplication.kt
+++ app/src/main/java/com/myapp/logistics/app/LogisticsApplication.kt
@@ -2,10 +2,10 @@ package com.myapp.logistics.app
 
 import android.app.Application
 import android.content.Context
-import android.util.Log
 import com.here.sdk.core.engine.SDKNativeEngine
 import com.here.sdk.core.engine.SDKOptions
 import com.here.sdk.core.errors.InstantiationErrorException
+import com.myapp.logistics.R
 import com.myapp.logistics.util.RemoteConfigUtils
 import dagger.hilt.android.HiltAndroidApp
 
@@ -14,10 +14,11 @@ class LogisticsApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         RemoteConfigUtils.init()
+        initializeHERESDK()
     }
     private fun initializeHERESDK() {
-        val accessKeyID = "MD54oUgWn8rzz8sK41k1mg"
-        val accessKeySecret = "oyDIIjuI7z7a7kH5fgCCOUnK4_3U_u8UJ_0KzaraeeBTi_nXekBYhTfsv615B-lOMnMgXkPApEzqhUF3VEo7Sw"
+        val accessKeyID = getString(R.string.here_sdk_api_key_value)
+        val accessKeySecret = getString(R.string.here_sdk_api_key_value)
         val options = SDKOptions(accessKeyID, accessKeySecret)
         try {
             val context: Context = this
